23c08526fe729d7ec66219469338d8f544576151
RaphaelOliveiraMoura <raphaeldeoliveiramoura@gmail.com>
2019-12-22 10:06:57 -0300

    feat: Add email to forgot password

--- app/Controllers/Http/ForgotPasswordController.js
+++ app/Controllers/Http/ForgotPasswordController.js
@@ -1,5 +1,7 @@
 const crypto = require('crypto')
+
 const User = use('App/Models/User')
+const Mail = use('Mail')
 
 class ForgotPasswordController {
   async store ({ request, response }) {
@@ -12,6 +14,21 @@ class ForgotPasswordController {
       user.token_created_at = new Date()
 
       await user.save()
+
+      await Mail.send(
+        ['emails.forgot_password'],
+        {
+          email,
+          token: user.token,
+          link: `${request.input('redirect_url')}?token=${user.token}`
+        },
+        message => {
+          message
+            .to(user.email)
+            .from('adonis@rocketseat.com.br', 'Adonis | Rocketseat')
+            .subject('Recuperação de senha')
+        }
+      )
     } catch (error) {
       return response
         .status(error.status)
--- start/app.js
+++ start/app.js
@@ -1,5 +1,3 @@
-'use strict'
-
 /*
 |--------------------------------------------------------------------------
 | Providers
@@ -15,7 +13,10 @@ const providers = [
   '@adonisjs/auth/providers/AuthProvider',
   '@adonisjs/bodyparser/providers/BodyParserProvider',
   '@adonisjs/cors/providers/CorsProvider',
-  '@adonisjs/lucid/providers/LucidProvider'
+  '@adonisjs/lucid/providers/LucidProvider',
+
+  '@adonisjs/mail/providers/MailProvider',
+  '@adonisjs/framework/providers/ViewProvider'
 ]
 
 /*
