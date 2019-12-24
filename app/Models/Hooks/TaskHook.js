79b2aafddbd7d5ac0ef7b66e4d174c327c0ff58b
RaphaelOliveiraMoura <raphaeldeoliveiramoura@gmail.com>
2019-12-24 18:28:57 -0300

    feat: Add task hooks to send mail

---
+++ app/Models/Hooks/TaskHook.js
@@ -0,0 +1,35 @@
+const Mail = use('Mail')
+const Helpers = use('Helpers')
+
+const TaskHook = (exports = module.exports = {})
+
+TaskHook.sendNewTaskMail = async taskInstance => {
+  if (!taskInstance.user_id && !taskInstance.dirty.user_id) return
+
+  const { email, username } = await taskInstance.user().fetch()
+
+  const file = await taskInstance.file().fetch()
+
+  const { title } = taskInstance
+
+  await Mail.send(
+    ['emails.new_task'],
+    {
+      username,
+      title,
+      hasAttachment: !!file
+    },
+    message => {
+      message
+        .to(email)
+        .from('adonis@rocketseat.com.br', 'Adonis | Rocketseat')
+        .subject('Nova tarefa para você')
+
+      if (file) {
+        message.attach(Helpers.tmpPath(`uploads/${file.file}`), {
+          filename: file.name
+        })
+      }
+    }
+  )
+};
